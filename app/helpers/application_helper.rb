e1521445c6e6bd6eabf732e7daf21ce10cf6ae48
thiaguerd <mail@thiago.pro>
2020-10-17 11:23:14 -0500

    add helpers current_user and user_authenticated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_user
+    User.find(session[:user]) if session[:user]
+  end
+
+  def user_authenticated
+    redirect_to root_path unless current_user
+  end
 end
