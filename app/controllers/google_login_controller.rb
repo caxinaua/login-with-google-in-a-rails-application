f1102403ba72d2286f71be1352d8b415d41e857c
thiaguerd <mail@thiago.pro>
2020-10-17 11:33:20 -0500

    create a GoogleLoginController and implements method callback

---
+++ app/controllers/google_login_controller.rb
@@ -0,0 +1,24 @@
+class GoogleLoginController < ApplicationController
+  def callback
+    # keys: access_token expires_in scope token_type id_token
+    @auth = LoginWithGoogle::Api.auth code: params[:code]
+
+    # keys: id email verified_email name given_name family_name picture locale
+    @info = LoginWithGoogle::Api.info @auth
+
+    user = User.find_by(uid: @info['id'])
+    if user
+      user.update access_token: @auth['access_token']
+    else
+      user = User.create({ uid: @info['id'],
+                           access_token: @auth['access_token'],
+                           email: @info['email'],
+                           name: @info['name'],
+                           picture: @info['picture'],
+                           locale: @info['locale'],
+                           provider: 'google' })
+    end
+    session[:user] = user.id
+    redirect_to user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'callback', to: 'google_login#callback'
   resources :users
   root to: 'users#index'
 end
